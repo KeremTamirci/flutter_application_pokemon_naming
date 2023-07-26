a12ad1e3692aa4a834ce06ff664856cc05842d2f
KeremTamirci <kerem.tamirci@gmail.com>
2023-07-26 16:15:52 +0300

    Removed unnecessary code

--- lib/pages/generator_page.dart
+++ lib/pages/generator_page.dart
@@ -1,33 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/big_card.dart';
 import '../viewmodel/my_app_state.dart';
 
-// For Firestore
-final db = FirebaseFirestore.instance;
-final FirebaseAuth auth = FirebaseAuth.instance;
-
 class GeneratorPage extends StatelessWidget {
-  void databaseTest() {
-    final User? currentUser = auth.currentUser;
-    final uid = currentUser!.uid;
-    String? emailid = currentUser.email;
-
-    final user = <String, dynamic>{
-      "mail": emailid,
-      "uid": uid,
-    };
-
-    print("user: ${currentUser.toString()}");
-    print("uid: $uid");
-    print("mail: $emailid");
-    // db.collection("Users").add(user);
-    db.collection("Users").doc(uid).set({"mail": emailid, "uid": uid});
-  }
-
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
